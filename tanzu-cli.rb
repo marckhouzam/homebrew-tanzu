940b74666b63cd9b08324186848333fa4ad5c2dc
Vui Lam <vui@vmware.com>
2023-04-07 16:37:47 -0700

    Update for test 0.89.1 release
    Signed-off-by: Vui Lam <vui@vmware.com>

--- tanzu-cli.rb
+++ tanzu-cli.rb
@@ -4,11 +4,11 @@
 class TanzuCli < Formula
   desc "The core Tanzu command-line tool"
   homepage "https://github.com/vmware-tanzu/tanzu-cli"
-  version "0.82.5-alpha.0"
+  version "0.89.1"
   head "https://github.com/vmware-tanzu/tanzu-cli.git", branch: "main"
 
   checksums = {
-    "darwin-amd64" => "4550a48c8e01e39ec9bbc57e20fec8625f70749450fe67e19dc4c5454d7969c0",
+    "darwin-amd64" => "2f3deb0f0277ae1330600bcd785c5fc5b80c7501754a56c96365f4c6fd4a8e75",
     "linux-amd64"  => "c9967ea224a9b2cb0edd9a061a157e234b83ed4876757b1eada0f3025214e4b6",
   }
 
@@ -23,7 +23,7 @@ class TanzuCli < Formula
     $os = "linux"
   end
 
-  url "http://build-squid.eng.vmware.com/build/mts/release/bora-21567337/publish/lin64/tanzu-cli/tanzu_cli/tanzu-cli-darwin-amd64.tar.gz"
+  url "http://build-squid.eng.vmware.com/build/mts/release/bora-21571440/publish/lin64/tanzu-cli/tanzu_cli/tanzu-cli-darwin-amd64.tar.gz"
 #  url "https://github.com/marckhouzam/tanzu-cli/releases/download/v#{version}/tanzu-cli-#{$os}-#{$arch}.tar.gz"
   sha256 checksums["#{$os}-#{$arch}"]
 
